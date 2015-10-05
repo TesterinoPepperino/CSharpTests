416be9cfb29eee1cd6e43d4af9a29c2ee98b7aef
TesterinoPepperino <doctorvollgas@gmail.com>
2015-10-05 19:16:45 +0200

    Add lmao project

---
+++ lmao/lmao/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace lmao
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Reader r = new Reader ();
+			Console.WriteLine (r.text);
+			Writer w = new Writer("what's up?");
+			Console.WriteLine (); //Need to fix output
+			Console.ReadLine();
+		}
+	}
+}
---
+++ lmao/lmao/Writer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace lmao
+{
+	public class Writer
+	{
+		string text = null;
+		public Writer (string text)
+		{
+			this.text = text;
+		}
+			
+		private string modifytext() {
+			this.text = text + "aay lmao";
+			return text;
+		}
+	}
+}
+
